c8a54d65d1ee70fd2304c63dc79ead9fac698ef8
SarsDev <sarsdesenv@gmail.com>
2024-05-25 20:52:37 -0300

    Configurações para o Swagger apresentar mais dados adicionais

--- Program.cs
+++ Program.cs
@@ -1,19 +1,39 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options => 
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "PatrimWeb API",
+        Description = "Projeto de estudo: Criação de Web API usando ASP.Net Core 8 + Swagger + Arquitetura REST.",
+        TermsOfService = new Uri("https://github.com/sarsdev/webapi_aspnet8_patrimweb/blob/main/README.md"),
+        Contact = new OpenApiContact
+        {
+            Name = "Sars Dev",
+            Url = new Uri("https://github.com/sarsdev")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "GNU GENERAL PUBLIC LICENSE",
+            Url = new Uri("https://github.com/sarsdev/webapi_aspnet8_patrimweb/blob/main/LICENSE")
+        }
+    });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options => 
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });
 }
 
 app.UseHttpsRedirection();
